a7164fe609134795a0c0ef58b394eb83e5e5c5f6
Emmanuel Gacer <emmanuelqgacer@gmail.com>
2019-11-16 00:26:36 +0400

    Added registration of routes based on configuration

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,34 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+// Register routes using the routes configuration file
+$routes = require __DIR__.'/../app/config/routes.php';
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
+// Iterate through the routes configuration
+foreach($routes as $type => $route) {
+    // Register each grouped routes
+    if($type === "groups") {
+
+    }
+    else {
+        /**
+         * Register each routes
+         * $verb - is the HTTP verb
+         * $methods - array of routes
+         */
+        foreach($route["methods"] as $verb => $methods) {
+            /**
+             * $endpoint - is the URI
+             * $action - is the options such as the action, name, etc.
+             */
+            foreach($methods as $endpoint => $action) {
+                // Prepend the classname to the action
+                if(array_key_exists("uses", $action)) {
+                    $action["uses"] = "{$route['class']}@{$action['uses']}";
+                }
+                // Register the route
+                $router->$verb($endpoint, $action);
+            }
+        }
+    }
+}
