387625f0016c8d0fd12b147c11279e130dede791
Emmanuel Gacer <emmanuelqgacer@gmail.com>
2019-11-16 12:37:50 +0400

    Updated documentation

--- app/Services/AuthenticateSlack.php
+++ app/Services/AuthenticateSlack.php
@@ -5,14 +5,23 @@ namespace App\Services;
 use \Illuminate\Http\Request;
 
 /**
+ * Abstract Class AuthenticateSlack
  * 
+ * Services for Authenticating Slack API Requests
+ * 
+ * @author Emmanuel Gacer <emmanuelqgacer@gmail.com>
  */
 
 abstract class AuthenticateSlack
 {
-
+    /**
+     * Injects the current request to authentication class
+     * 
+     * @var Request $request The current HTTP Request
+     */
     public function __construct(Request $request)
     {
+        // This variable is defined from each authentication class
         $this->request = $request;
     }
 }
